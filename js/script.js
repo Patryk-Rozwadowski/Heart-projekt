3b1530ac4e06c36612cbefdab48b1b2a7869aa2b
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-05-30 01:02:34 +0200

    Add scrollTo script

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const aboutUs = document.querySelector('.about-us');
+const offer = document.querySelector('.offer');
+const description = document.querySelector('.description');
+const home = document.querySelector('.hero-container');
+const next = document.getElementById('next-section');
+const service = document.querySelector('.description');
+// MENU
+const aboutUSNav = document.getElementById('about-us');
+const offerNav = document.getElementById('offer');
+const homeNav = document.getElementById('home');
+const serviceNav = document.getElementById('service');
+
+scrollTo = (element) => {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: element.offsetTop - 150
+  });
+}
+
+next.addEventListener('click', function() {
+  scrollTo(aboutUs);
+});
+
+aboutUSNav.addEventListener('click', function() {
+  scrollTo(aboutUs);
+});
+
+offerNav.addEventListener('click', function() {
+  scrollTo(offer);
+});
+
+homeNav.addEventListener('click', function() {
+  scrollTo(home);
+});
+
+serviceNav.addEventListener('click', function() {
+  scrollTo(service);
+});
